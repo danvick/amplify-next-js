06c505d0a1806edef9fce339346ca5866f4cfa85
Danvick <danvickmiller@live.com>
2024-11-15 16:36:34 +0300

    feat: reverted config stuff

--- app/page.tsx
+++ app/page.tsx
@@ -7,14 +7,13 @@ import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
-import { camera } from '@biopassid/face-sdk';
+import { Authenticator } from "@aws-amplify/ui-react";
 
 Amplify.configure(outputs);
 
 const client = generateClient<Schema>();
 
 export default function App() {
-
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   function listTodos() {
@@ -24,13 +23,6 @@ export default function App() {
   }
 
   useEffect(() => {
-    const { takePicture } = camera();
-    async function handleTakePicture() {
-      const resp = await takePicture({
-        element: document.querySelector("#elementId") as HTMLElement,
-      })
-    }
-    handleTakePicture();
     listTodos();
   }, []);
 
@@ -42,21 +34,27 @@ export default function App() {
 
   return (
     <main>
-      {/* <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
-          Review next steps of this tutorial.
-        </a>
-      </div> */}
-      <div id="elementId"></div>
+      <Authenticator>
+        {({ signOut, user }) => (
+          <div>
+            <h1>My todos</h1>
+            <button onClick={createTodo}>+ new</button>
+            <ul>
+              {todos.map((todo) => (
+                <li key={todo.id}>{todo.content}</li>
+              ))}
+            </ul>
+            <div>
+              🥳 App successfully hosted. Try creating a new todo.
+              <br />
+              <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
+                Review next steps of this tutorial.
+              </a>
+            </div>
+            <div id="elementId"></div>
+          </div>
+        )}
+      </Authenticator>
     </main>
   );
 }
